6b9148b0882958c2a98b9b029078522e1a5aaacb
Maciej_Malecki_2Tc <154277219+MaciejTc@users.noreply.github.com>
2024-06-07 02:17:27 +0200

    Delete Constants.java

--- Constants.java
+++
@@ -1,7 +0,0 @@
-public class Constants {
-    public static final int SCREEN_WIDTH = 1000;
-    public static final int SCREEN_HEIGHT = 800;
-    public static final int TILE_SIZE = 40;
-    public static final int FLOOR_WIDTH = SCREEN_WIDTH / TILE_SIZE;
-    public static final int FLOOR_HEIGHT = SCREEN_HEIGHT / TILE_SIZE;
-}
